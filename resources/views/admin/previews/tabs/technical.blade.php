3ab87efe70534ce60ba22ebc1871e9f4ea518bb6
Amr Khaled <a.khalid@tocaan.com>
2019-04-25 00:31:03 +0300

    col6

--- resources/views/admin/previews/tabs/technical.blade.php
+++ resources/views/admin/previews/tabs/technical.blade.php
@@ -12,7 +12,7 @@
                 @foreach ($preview->details as $detail)
                 
                 <input type="hidden" name="service_id[]" value="{{ $detail->service_id }}">
-                <div class="col-lg-6 col-xs-12 col-sm-12">
+                <div class="col-lg-12 col-xs-12 col-sm-12">
                     <div class="portlet light ">
                         <div class="portlet-title tabbable-line">
                             <div class="caption">
@@ -23,67 +23,71 @@
                             </div>
                         </div>
                         <div class="portlet-body">
-                            <div class="mt-comments">
-                                @foreach (Preview::technicalOfService($preview) as $technical)
-                                <div class="mt-comment">
-                                    <div class="mt-comment-img">
-                                        <img src="{{ url($technical->image) }}" style="max-width: 48px;"/>
-                                    </div>
-                                    <div class="mt-comment-body">
-                                        <div class="mt-comment-info">
-                                            <span class="mt-comment-author">
-                                                {{ $technical->name }}
-                                            </span>
-                                        </div>
-                                        <div class="mt-comment-text">
-                                            <li>المناطق التي يخدمها :
-                                                @foreach ($technical->locationsOfTechnical as $location)
-                                                {{ $loop->first ? '' : ' , ' }}
-                                                {{ $location->name_ar }}
-                                                @endforeach
-                                            </li>
-                                            <li>ايام العمل :
-                                                @foreach ($technical->workDays as $day)
-                                                {{ $loop->first ? '' : ' , ' }}
-                                                {{ $day->day }}
-                                                @endforeach
-                                            </li>
-                                            <li>الخدمات التي يعمل بها :
-                                                @foreach ($technical->servicesOfTechnical as $service)
-                                                {{ $loop->first ? '' : ' , ' }}
-                                                {{ $service->name_ar }}
-                                                @endforeach
-                                            </li>
-                                            <li>يبدا دوام من  : {{ $technical->shift->from }}</li>
-                                            <li>ينتهي دوام في : {{ $technical->shift->to }}</li>
-                                            @if (Preview::previewInSameDate($technical,$preview))
-                                            <li class="alert alert-danger">
-                                                يمتلك الفني طلب معاينة اخرة في نفس الموعد
-                                            </li>
-                                            @endif
-                                        </div>
-                                        @if (!Preview::previewInSameDate($technical,$preview))
-                                        <div class="mt-comment-details">
-                                            <div class="form-group">
-                                                <div class="mt-radio-inline">
-                                                    <label class="mt-radio mt-radio-outline">
-                                                        <input type="radio" name="technical[]"
-                                                        value="{{ $technical->id }}"
-                                                        @if ($preview->technical)
-                                                        @if ($technical->id == $preview->technical->user_id)
-                                                        checked=""
-                                                        @endif
-                                                        @endif>
-                                                        <span></span>
-                                                    </label>
+                            <div class="row">
+                                <div class="mt-comments">
+                                    @foreach (Preview::technicalOfService($preview) as $technical)
+                                    <div class="col-md-6">
+                                        <div class="mt-comment">
+                                            <div class="mt-comment-img">
+                                                <img src="{{ url($technical->image) }}" style="max-width: 48px;"/>
+                                            </div>
+                                            <div class="mt-comment-body">
+                                                <div class="mt-comment-info">
+                                                    <span class="mt-comment-author">
+                                                        {{ $technical->name }}
+                                                    </span>
                                                 </div>
+                                                <div class="mt-comment-text">
+                                                    <li>المناطق التي يخدمها :
+                                                        @foreach ($technical->locationsOfTechnical as $location)
+                                                        {{ $loop->first ? '' : ' , ' }}
+                                                        {{ $location->name_ar }}
+                                                        @endforeach
+                                                    </li>
+                                                    <li>ايام العمل :
+                                                        @foreach ($technical->workDays as $day)
+                                                        {{ $loop->first ? '' : ' , ' }}
+                                                        {{ $day->day }}
+                                                        @endforeach
+                                                    </li>
+                                                    <li>الخدمات التي يعمل بها :
+                                                        @foreach ($technical->servicesOfTechnical as $service)
+                                                        {{ $loop->first ? '' : ' , ' }}
+                                                        {{ $service->name_ar }}
+                                                        @endforeach
+                                                    </li>
+                                                    <li>يبدا دوام من  : {{ $technical->shift->from }}</li>
+                                                    <li>ينتهي دوام في : {{ $technical->shift->to }}</li>
+                                                    @if (Preview::previewInSameDate($technical,$preview))
+                                                    <li class="alert alert-danger">
+                                                        يمتلك الفني طلب معاينة اخرة في نفس الموعد
+                                                    </li>
+                                                    @endif
+                                                </div>
+                                                @if (!Preview::previewInSameDate($technical,$preview))
+                                                <div class="mt-comment-details">
+                                                    <div class="form-group">
+                                                        <div class="mt-radio-inline">
+                                                            <label class="mt-radio mt-radio-outline">
+                                                                <input type="radio" name="technical[]"
+                                                                value="{{ $technical->id }}"
+                                                                @if ($preview->technical)
+                                                                @if ($technical->id == $preview->technical->user_id)
+                                                                checked=""
+                                                                @endif
+                                                                @endif>
+                                                                <span></span>
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                @endif
+                                                
                                             </div>
                                         </div>
-                                        @endif
-                                        
                                     </div>
+                                    @endforeach
                                 </div>
-                                @endforeach
                             </div>
                         </div>
                     </div>
