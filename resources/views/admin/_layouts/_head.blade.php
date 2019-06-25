3069cde4d4d13eb256778509620c70800a499d4b
akhalid4493 <amrkhaled@Amrs-MacBook-Pro.local>
2019-06-25 11:19:47 +0300

    live env

--- resources/views/admin/_layouts/_head.blade.php
+++ resources/views/admin/_layouts/_head.blade.php
@@ -1,7 +1,7 @@
 <head>
 	<meta charset="utf-8" />
 	<link rel="shortcut icon" href="{{url(settings('favicon'))}}">
-	<title>@yield('title','الرئيسية') || FIXPRO STG</title>
+	<title>@yield('title','الرئيسية') || FIXPRO</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="width=device-width, initial-scale=1" name="viewport" />
 
